78e9e8d8ee850e26e7ddbdfb7b7560e4a5105c99
vv_tomilin <backatom1@gmail.com>
2021-12-02 11:39:22 +0500

    Сделал бургер меню для мобильной версии приложения и стилизовал его, так же реализовал его открытие и закрытие

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,47 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
 
-  const activeTabStyles = 'app__navigation-tab app__navigation-tab--active';
+  const [burgerMenuToggle, setBurgerMenuToggle] = useState(false);
+
+  const activeTabStyles = `header__navigation-tab header__navigation-tab--active`;
+  const burgerMenuStyles = 'header__burger-menu--opened';
+  const navigationCloseStyles = 'header__navigation--closed';
+
+  const clickBurgerMenu = () => {
+    setBurgerMenuToggle(!burgerMenuToggle);
+  };
 
   return (
-    <nav className='app__navigation'>
-      <NavLink
-        to="/"
-        className={({ isActive }) => isActive ? activeTabStyles : 'app__navigation-tab'}
-      >
-        Конверер
-      </NavLink>
-      <NavLink
-        to="/currencys"
-        className={({ isActive }) => isActive ? activeTabStyles : 'app__navigation-tab'}
-      >
-        Курсы валют
-      </NavLink>
-      <NavLink
-        to="/cripto"
-        className={({ isActive }) => isActive ? activeTabStyles : 'app__navigation-tab'}
-      >
-        Курсы криптовалют
-      </NavLink>
-    </nav>
+    <>
+      <div
+        className={burgerMenuToggle ? burgerMenuStyles : 'header__burger-menu'}
+        onClick={clickBurgerMenu}></div>
+      <nav className={burgerMenuToggle ? 'header__navigation' : navigationCloseStyles}>
+        <NavLink
+          to="/"
+          onClick={clickBurgerMenu}
+          className={({ isActive }) => isActive ? activeTabStyles : 'header__navigation-tab'}
+        >
+          Конвертер
+        </NavLink>
+        <NavLink
+          to="/currencys"
+          onClick={clickBurgerMenu}
+          className={({ isActive }) => isActive ? activeTabStyles : 'header__navigation-tab'}
+        >
+          Курсы валют
+        </NavLink>
+        <NavLink
+          to="/cripto"
+          onClick={clickBurgerMenu}
+          className={({ isActive }) => isActive ? activeTabStyles : 'header__navigation-tab'}
+        >
+          Курсы криптовалют
+        </NavLink>
+      </nav>
+    </>
   );
 };
 
